991414c28efb7e081ac333fde3633ba27b884c22
Yufan You <ouuansteve@gmail.com>
2025-04-30 14:28:01 +0800

    feat: add many rules
    basically derived from the airbnb rules with my opinions

--- packages/ts/index.js
+++ packages/ts/index.js
@@ -1,8 +1,9 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
+import { globalIgnores } from 'eslint/config';
 import importPlugin from 'eslint-plugin-import';
 import * as regexpPlugin from 'eslint-plugin-regexp';
+import tseslint from 'typescript-eslint';
 
 function withName(config, name) {
   return {
@@ -12,6 +13,11 @@ function withName(config, name) {
 }
 
 export default tseslint.config(
+  globalIgnores([
+    '**/dist/',
+    '**/public/',
+    '**/static/',
+  ], '@ouuan/ignores'),
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
@@ -33,53 +39,131 @@ export default tseslint.config(
   importPlugin.flatConfigs.recommended,
   withName(importPlugin.flatConfigs.typescript, 'import/typescript'),
   {
-    name: '@ouuan/ts-custom',
+    name: '@ouuan/enable',
     rules: {
+      '@stylistic/array-bracket-newline': ['error', 'consistent'],
+      '@stylistic/function-call-argument-newline': ['error', 'consistent'],
       '@stylistic/function-call-spacing': 'error',
+      '@stylistic/function-paren-newline': ['error', 'multiline-arguments'],
+      '@stylistic/linebreak-style': 'error',
       '@stylistic/max-len': ['error', {
         code: 100,
         ignoreUrls: true,
+      }],
+      '@stylistic/newline-per-chained-call': ['error', {
+        ignoreChainWithDepth: 3,
+      }],
+      '@stylistic/object-curly-newline': ['error', {
+        minProperties: 4,
+        consistent: true,
+      }],
+      '@stylistic/object-property-newline': ['error', {
+        allowAllPropertiesOnSameLine: true,
       }],
       '@stylistic/quotes': ['error', 'single', {
         avoidEscape: true,
         allowTemplateLiterals: 'avoidEscape',
       }],
+      '@stylistic/semi-style': 'error',
+      '@stylistic/switch-colon-spacing': 'error',
 
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/class-methods-use-this': ['error', {
+        ignoreOverrideMethods: true,
+        ignoreClassesThatImplementAnInterface: 'public-fields',
+      }],
+      '@typescript-eslint/default-param-last': 'error',
       '@typescript-eslint/prefer-nullish-coalescing': ['error', {
         ignorePrimitives: {
           string: true,
           boolean: true,
         },
       }],
-      '@typescript-eslint/prefer-regexp-exec': 'off',
       '@typescript-eslint/restrict-template-expressions': ['error', {
         allowBoolean: false,
         allowNever: false,
         allowNullish: false,
       }],
 
-      'import/no-unresolved': 'off',
       'import/no-extraneous-dependencies': [
         'error',
         {
           devDependencies: [
-            '**/*.{config,spec}.{js,ts}',
+            '**/*.{config,spec,test}.{js,ts}',
             '**/*.d.ts',
           ],
           optionalDependencies: false,
         },
       ],
+      'import/no-mutable-exports': 'error',
+      'import/order': ['error', {
+        alphabetize: { order: 'asc' },
+      }],
+      'import/newline-after-import': 'error',
+      'import/no-absolute-path': 'error',
+      'import/no-dynamic-require': 'error',
+      'import/no-named-default': 'error',
+      'import/no-useless-path-segments': 'error',
 
+      'array-callback-return': 'error',
+      'camelcase': ['error', {
+        properties: 'never',
+        ignoreDestructuring: true,
+        ignoreImports: true,
+      }],
+      'default-case-last': 'error',
+      'eqeqeq': 'error',
+      'grouped-accessor-pairs': 'error',
+      'guard-for-in': 'error',
+      'object-shorthand': ['error', 'always', { avoidQuotes: true }],
+      'one-var': ['error', { initialized: 'never' }],
+      'operator-assignment': 'error',
+      'prefer-arrow-callback': 'error',
+      'prefer-destructuring': ['error', {
+        VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+        AssignmentExpression: {
+          array: true,
+          object: false,
+        },
+      }],
+      'prefer-exponentiation-operator': 'error',
+      'prefer-numeric-literals': 'error',
+      'prefer-template': 'error',
+      'radix': 'error',
+
+      'no-await-in-loop': 'warn',
+      'no-bitwise': 'warn',
+      'no-caller': 'error',
       'no-console': 'warn',
+      'no-constructor-return': 'error',
+      'no-eval': 'error',
+      'no-extend-native': 'error',
+      'no-extra-bind': 'error',
+      'no-extra-label': 'error',
+      'no-implicit-coercion': 'error',
+      'no-iterator': 'error',
+      'no-lone-blocks': 'error',
+      'no-multi-assign': ['error', { ignoreNonDeclaration: true }],
+      'no-nested-ternary': 'error',
+      'no-new-func': 'error',
+      'no-new-wrappers': 'error',
+      'no-octal-escape': 'error',
       'no-param-reassign': 'error',
+      'no-proto': 'error',
+      'no-restricted-exports': ['error', {
+        restrictedNamedExports: ['default', 'then'],
+      }],
       'no-restricted-syntax': [
         'error',
         'ForInStatement',
         'LabeledStatement',
         'WithStatement',
         {
+          // eslint-disable-next-line @stylistic/max-len
           selector: 'CallExpression[callee.property.name=\'replace\'] > .arguments:nth-child(2):not(Literal):not(ArrowFunctionExpression):not(FunctionExpression)',
+          // eslint-disable-next-line @stylistic/max-len
           message: 'Only literals and functions are permitted as the 2nd argument of String.prototype.replace. Use a function that returns the expression instead.',
         },
         {
@@ -87,11 +171,29 @@ export default tseslint.config(
           message: 'Use replace with /regex/g instead.',
         },
       ],
+      'no-script-url': 'error',
+      'no-sequences': 'error',
+      'no-unreachable-loop': ['error', {
+        ignore: ['DoWhileStatement'],
+      }],
+      'no-useless-computed-key': 'error',
+      'no-useless-concat': 'error',
+      'no-useless-rename': 'error',
+      'no-useless-return': 'error',
     },
   },
   {
     ...tseslint.configs.disableTypeChecked,
     files: ['**/*.js'],
     name: '@ouuan/disable-type-checked-lint-for-js',
+  },
+  {
+    name: '@ouuan/disable',
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/prefer-regexp-exec': 'off',
+      'import/no-unresolved': 'off',
+      'no-undef': 'off', // already disabled for TS, but also false positive for JS
+    },
   },
 );
--- packages/vue/index.js
+++ packages/vue/index.js
@@ -1,32 +1,90 @@
-import pluginVue from 'eslint-plugin-vue';
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 import ouuan from '@ouuan/eslint-config-ts';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
 
 export default defineConfigWithVueTs(
   ...ouuan,
   pluginVue.configs['flat/recommended'],
   vueTsConfigs.strictTypeChecked,
+  vueTsConfigs.stylisticTypeChecked,
   {
-    name: '@ouuan/ts-custom-override',
+    name: '@ouuan/vue-override',
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
   {
-    name: '@ouuan/vue-custom',
+    name: '@ouuan/vue',
     files: ['**/*.vue'],
     rules: {
       '@typescript-eslint/prefer-function-type': 'off', // for defineEmits
+      'vue/array-bracket-newline': ['error', 'consistent'],
+      'vue/array-bracket-spacing': 'error',
+      'vue/arrow-spacing': 'error',
+      'vue/block-spacing': 'error',
+      'vue/block-tag-newline': 'error',
+      'vue/comma-dangle': ['error', 'always-multiline'],
+      'vue/comma-spacing': 'error',
+      'vue/comma-style': 'error',
+      'vue/component-api-style': ['error', ['script-setup']],
       'vue/component-name-in-template-casing': [
         'error',
         'kebab-case',
         { registeredComponentsOnly: false },
       ],
+      'vue/dot-notation': 'error',
+      'vue/eqeqeq': 'error',
+      'vue/func-call-spacing': 'error',
+      'vue/key-spacing': 'error',
+      'vue/max-len': ['error', {
+        code: 100,
+        ignoreUrls: true,
+      }],
+      'vue/multiline-ternary': ['error', 'always-multiline'],
+      'vue/no-constant-condition': 'error',
+      'vue/no-implicit-coercion': 'error',
+      'vue/no-irregular-whitespace': 'error',
+      'vue/no-loss-of-precision': 'error',
+      'vue/no-restricted-syntax': [
+        'error',
+        {
+          selector: 'VElement > VExpressionContainer CallExpression',
+          message: 'Use `computed` instead of calling functions in template',
+        },
+        {
+          // eslint-disable-next-line @stylistic/max-len
+          selector: 'CallExpression[callee.property.name=\'replace\'] > .arguments:nth-child(2):not(Literal):not(ArrowFunctionExpression):not(FunctionExpression)',
+          // eslint-disable-next-line @stylistic/max-len
+          message: 'Only literals and functions are permitted as the 2nd argument of String.prototype.replace. Use a function that returns the expression instead.',
+        },
+        {
+          selector: 'CallExpression[callee.property.name=\'replaceAll\']',
+          message: 'Use replace with /regex/g instead.',
+        },
+      ],
+      'vue/no-sparse-arrays': 'error',
       'vue/no-unused-refs': 'warn',
+      'vue/no-useless-concat': 'error',
       'vue/no-useless-mustaches': 'error',
       'vue/no-useless-v-bind': 'error',
       'vue/no-v-text': 'error',
+      'vue/object-curly-newline': ['error', {
+        minProperties: 4,
+        consistent: true,
+      }],
+      'vue/object-curly-spacing': ['error', 'always'],
+      'vue/object-property-newline': ['error', {
+        allowAllPropertiesOnSameLine: true,
+      }],
+      'vue/object-shorthand': ['error', 'always', { avoidQuotes: true }],
+      'vue/operator-linebreak': ['error', 'before'],
       'vue/padding-line-between-blocks': 'error',
+      'vue/prefer-template': 'error',
+      'vue/quote-props': ['error', 'consistent-as-needed'],
+      'vue/space-in-parens': ['error', 'never'],
+      'vue/space-infix-ops': 'error',
+      'vue/space-unary-ops': 'error',
+      'vue/template-curly-spacing': 'error',
       'vue/v-for-delimiter-style': ['error', 'of'],
       'vue/no-boolean-default': ['error', 'default-false'],
     },
